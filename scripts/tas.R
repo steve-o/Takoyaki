7c8f8629eb9d9964d3c406e48805cd0735736d29
Steven McCoy <fnjordy@gmail.com>
2015-08-26 12:00:17 -0400

    RJSONIO supports inline null translation. http://www.inside-r.org/packages/cran/RJSONIO/docs/fromJSON

--- scripts/tas.R
+++ scripts/tas.R
@@ -2,12 +2,8 @@ library(RJSONIO)
 library(RCurl)
 
 op <- options(digits.secs=3)
-list <- fromJSON(getURL("http://nylabdev5:8000/FB.O/tas?interval=2015-06-11T05:00:00.000Z/P5D"))
+list <- fromJSON(getURL("http://nylabdev5:8000/FB.O/tas?interval=2015-06-11T05:00:00.000Z/P5D"), nullValue = NA)
 list$timeseries[[2]] <- strptime(list$timeseries[[1]], "%Y-%m-%dT%H:%M:%OSZ", tz="GMT")
-timeseries <- lapply(list$timeseries, function(x) {
-	x[sapply(x, is.null)] <- NA
-	unlist(x)
-})
-df <- data.frame(timeseries)
+df <- data.frame(list$timeseries)
 names(df) <- list$fields
 print(df)
