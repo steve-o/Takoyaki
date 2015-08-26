0b95bfbb1145e70fcb323a2b6b96b01e96f76b19
Steven McCoy <fnjordy@gmail.com>
2015-08-26 10:35:48 -0400

    Add Elektron Time Series to Swagger spec.

---
+++ scripts/tas.R
@@ -0,0 +1,9 @@
+library(RJSONIO)
+library(RCurl)
+
+op <- options(digits.secs=3)
+list <- fromJSON(getURL("http://nylabdev5:8000/FB.O/tas?interval=2015-06-11T05:00:00.000Z/P5D"))
+list$timeseries[[1]] <- strptime(list$timeseries[[1]], "%Y-%m-%dT%H:%M:%OSZ", tz="GMT")
+df <- data.frame(list$timeseries)
+names(df) <- list$fields
+print(df)
