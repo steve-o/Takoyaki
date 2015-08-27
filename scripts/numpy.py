ab00f3cb317409b504e88c69ae20116df3131a88
Steven McCoy <fnjordy@gmail.com>
2015-08-27 16:36:36 -0400

    Revert URL in NumPy to internal server for testing. Convert datetime field in Pandas from string.

--- scripts/numpy.py
+++ scripts/numpy.py
@@ -3,6 +3,6 @@ import urllib
 import json
 import numpy as np
 
-result = json.load(urllib.urlopen("http://miru.hk/tmp/FB.O.json"))
+result = json.load(urllib.urlopen("http://nylabdev5:8000/FB.O/days?interval=2015-06-11T05:00:00.000Z/P5D"))
 r = np.core.records.fromarrays(result['timeseries'], names = result['fields'])
 print r
--- scripts/pandas.py
+++ scripts/pandas.py
@@ -6,4 +6,5 @@ import pandas as pd
 
 result = json.load(urllib.urlopen("http://nylabdev5:8000/FB.O/days?interval=2015-06-11T05:00:00.000Z/P5D"))
 df = pd.DataFrame.from_items(zip(result['fields'], result['timeseries']))
+df.datetime = df.datetime.astype("datetime64")
 print df
