e980e33b71d3d2b7610441c10ad3242629aa13a1
Steven McCoy <fnjordy@gmail.com>
2015-08-27 20:53:22 -0400

    Update Julia usage per https://github.com/quinnj/Dates.jl/issues/28#issuecomment-135590148

--- scripts/daily.jl
+++ scripts/daily.jl
@@ -5,6 +5,5 @@ using Dates;
 
 json = JSON.parse(bytestring(get("http://nylabdev5:8000/FB.O/days?interval=2015-06-11T05:00:00.000Z/P5D").body.data));
 df = DataFrame(json["timeseries"], convert(Array{Symbol}, json["fields"]));
-# Julia 0.3 does not support decoding ISO 8601 Zulu timestamps.
-df[1] = convert(Array{DateTime}, map(x -> DateTime(rstrip(x, Set{Char}('Z'))), df[1]));
+df[1] = convert(Array{DateTime}, map(x -> DateTime(x, "yyyy-mm-ddTHH:MM:SS.sZ"), df[1]));
 println(df);
